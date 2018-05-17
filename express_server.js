85a4f3e3bb6bc078c3655e88dac9e47fd67835fe
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-17 21:41:49 +0000

    added a new GET route which redirects to the longURL

--- express_server.js
+++ express_server.js
@@ -1,24 +1,46 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 //define middleware
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let randomString = "";
+  let newString = "abcdefghijkl5896y5y9yr498y43973455895654743ght";
+  for(var i = 0; i < 5; i++) {
+    var random = Math.floor(Math.random() * newString.length - 1);
+    randomString += newString[random];
+  }
+  return randomString;
+}
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+let longURL = urlDatabase[req.params.shortURL];
+  console.log(req.params);
+  res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -28,8 +50,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let newShortUrl = generateRandomString();
+  urlDatabase[newShortUrl] = longURL;
+  res.redirect('/urls');
+});
+
+
+app.post("/urls:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
